80bd757e52ac45bbc05ac3ba8ad4b4f8dced6b21
Igor Martynovich <ihor.martynovich@gmail.com>
2017-05-05 12:10:19 +0300

    Second commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Igor on 05.05.2017.
+ */
+public class Calculator {
+
+    int sum(int a, int b) {
+        return a+b;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Jenkins!");
+        int a = 2;
+        int b = 2;
+        System.out.println(a + "+" + b + "=" + new Calculator().sum(a,b));
     }
 }
